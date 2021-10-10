bf1c7929d717856779a61d09c5d8cdc965c42480
KEYUCUILAVINIA <kcui0004@student.monash.edu>
2021-10-10 20:59:58 +0800

    Add project files.

---
+++ FIT5032_2021S2/Models/BookEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FIT5032_2021S2.Models
+{
+    public class BookEvent
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int StoreEventId { get; set; }
+        public StoreEvent StoreEvent { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
